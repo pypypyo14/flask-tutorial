b48ec9b282e4c7219eadce4597ead81afd9ca1f0
Saori <jkmr9214@gmail.com>
2019-07-16 23:32:47 +0900

    add blog blueprint

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,6 +4,7 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 def get_db():
+    print('getdb')
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
@@ -11,7 +12,7 @@ def get_db():
         )
         g.db.row_factory = sqlite3.Row
 
-        return g.db
+    return g.db
 
 def close_db(e=None):
     db = g.pop('db', None)
